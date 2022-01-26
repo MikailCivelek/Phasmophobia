e34a2268c471f4685b3875846c6d291047e9cab1
MikailCivelekPXL <Mikail.civelek@student.pxl.be>
2022-01-26 13:52:19 +0100

    Quick update to fix to latest release

---
+++ Phasmo/Phasmo/Evidence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phasmo
+{
+    static class Evidence
+    {
+        public const string EMF5 = "EMF5";
+        public const string SpiritBox = "Spirit Box";
+        public const string Fingerprints = "Fingerprints";
+        public const string GhostOrb = "Ghost Orb";
+        public const string GhostWriting = "Ghost Writing";
+        public const string FreezingTemperatures = "Freezing Temperatures";
+        public const string DotsProjector = "D.O.T.S Projector";
+    }
+}
--- Phasmo/Phasmo/MainWindow.xaml.cs
+++ Phasmo/Phasmo/MainWindow.xaml.cs
@@ -20,12 +20,11 @@ namespace Phasmo
     /// </summary>
     public partial class MainWindow : Window
     {
-        enum Evidences { EMF5, Spirit_Box, Fingerprints, Ghost_Orb, Ghost_Writing, Freezing_Temperatures, DOTS_Projector}
         private string _FirstEvidence = "";
         private string _SecondEvidence = "";
         private string _ThirdEvidence = "";
-        private List<string> Evidence = new List<string>() { "EMF5", "Spirit Box", "Fingerprints", "Ghost Orb", "Ghost Writing", "Freezing Temperatures", "D.O.T.S Projector" };
-        private List<string> EvidenceNotNeededHelper;
+        private List<string> Evidences = new List<string>() { Evidence.EMF5, Evidence.SpiritBox, Evidence.Fingerprints, Evidence.GhostOrb, Evidence.GhostWriting, Evidence.FreezingTemperatures, Evidence.DotsProjector};
+        private List<string> EvidenceNotNeeded;
         private List<GhostType> Ghosts = new List<GhostType>();
         private List<GhostType> Potential;
         private string[] Missing;
@@ -35,51 +34,54 @@ namespace Phasmo
             InitializeComponent();
 
             // ADDING ALL GHOSTS TO LIST
+            Ghosts.Add(new GhostType("Spirit", Evidence.EMF5, Evidence.SpiritBox, Evidence.GhostWriting, "Smudge sticks stop attacks for extra 90 seconds", "None"));
+            Ghosts.Add(new GhostType("Wraith", Evidence.EMF5, Evidence.SpiritBox, Evidence.DotsProjector, "Toxic reaction to salt", "Can't be tracked by footprints"));
+            Ghosts.Add(new GhostType("Phantom", Evidence.Fingerprints, Evidence.SpiritBox, Evidence.DotsProjector, "Taking a photo will make it go away for a little while", "Sanity drops quickly by looking at it"));
+            Ghosts.Add(new GhostType("Poltergeist", Evidence.GhostWriting, Evidence.SpiritBox, Evidence.Fingerprints, "Ineffective in an empty room", "Can throw multiple objects at the same time"));
 
-            Ghosts.Add(new GhostType("Spirit", "EMF5", "Spirit Box", "Ghost Writing", "Smudge sticks stop attacks for extra 90 seconds", "None"));
-            Ghosts.Add(new GhostType("Wraith", "EMF5", "Spirit Box", "D.O.T.S Projector", "Toxic reaction to salt", "Can't be tracked by footprints"));
-            Ghosts.Add(new GhostType("Phantom", "Fingerprints", "Spirit Box", "D.O.T.S Projector", "Taking a photo will make it go away for a little while", "Sanity drops quickly by looking at it"));
-            Ghosts.Add(new GhostType("Poltergeist", "Ghost Writing", "Spirit Box", "Fingerprints", "Ineffective in an empty room", "Can throw multiple objects at the same time"));
+            Ghosts.Add(new GhostType("Banshee", Evidence.Fingerprints, Evidence.GhostOrb, Evidence.DotsProjector, "Less aggressive around Crucifix", "Only targets one person at a time"));
+            Ghosts.Add(new GhostType("Jinn", Evidence.EMF5, Evidence.FreezingTemperatures, Evidence.Fingerprints, "Turning off the power disables ability", "Travels at faster speed if victim is far away"));
+            Ghosts.Add(new GhostType("Mare", Evidence.GhostOrb, Evidence.SpiritBox, Evidence.GhostWriting, "Turning on lights lowers chance of attack", "Increased chance of attack in the dark"));
+            Ghosts.Add(new GhostType("Revenant", Evidence.GhostOrb, Evidence.GhostWriting, Evidence.FreezingTemperatures, "Hiding will make it move more slowly", "While hunting, it will travel faster"));
 
-            Ghosts.Add(new GhostType("Banshee", "Fingerprints", "Ghost Orb", "D.O.T.S Projector", "Less aggressive around Crucifix", "Only targets one person at a time"));
-            Ghosts.Add(new GhostType("Jinn", "EMF5", "Freezing Temperatures", "Fingerprints", "Turning off the power disables ability", "Travels at faster speed if victim is far away"));
-            Ghosts.Add(new GhostType("Mare", "Ghost Orb", "Spirit Box", "Ghost Writing", "Turning on lights lowers chance of attack", "Increased chance of attack in the dark"));
-            Ghosts.Add(new GhostType("Revenant", "Ghost Orb", "Ghost Writing", "Freezing Temperatures", "Hiding will make it move more slowly", "While hunting, it will travel faster"));
+            Ghosts.Add(new GhostType("Shade", Evidence.EMF5, Evidence.GhostWriting, Evidence.FreezingTemperatures, "Will not hunt when multiple players are around", "Being shy -> Hard to find"));
+            Ghosts.Add(new GhostType("Demon", Evidence.FreezingTemperatures, Evidence.GhostWriting, Evidence.Fingerprints, "Ouija board answers take less sanity (succesful)", "Will attack more often than any other ghost"));
+            Ghosts.Add(new GhostType("Yurei", Evidence.GhostOrb, Evidence.FreezingTemperatures, Evidence.DotsProjector, "Smudging its room keeps the ghost from wandering", "Strong effect on sanity"));
+            Ghosts.Add(new GhostType("Oni", Evidence.EMF5, Evidence.FreezingTemperatures, Evidence.DotsProjector, "Very active -> Easy to identify", "More active when players are around"));
 
-            Ghosts.Add(new GhostType("Shade", "EMF5", "Ghost Writing", "Freezing Temperatures", "Will not hunt when multiple players are around", "Being shy -> Hard to find"));
-            Ghosts.Add(new GhostType("Demon", "Freezing Temperatures", "Ghost Writing", "Fingerprints", "Ouija board answers take less sanity (succesful)", "Will attack more often than any other ghost"));
-            Ghosts.Add(new GhostType("Yurei", "Ghost Orb", "Freezing Temperatures", "D.O.T.S Projector", "Smudging its room keeps the ghost from wandering", "Strong effect on sanity"));
-            Ghosts.Add(new GhostType("Oni", "EMF5", "Freezing Temperatures", "D.O.T.S Projector", "Very active -> Easy to identify", "More active when players are around"));
+            Ghosts.Add(new GhostType("Yokai", Evidence.GhostOrb, Evidence.SpiritBox, Evidence.DotsProjector, "While hunting, it can only hear voices near it", "Talking near the ghost makes it to hunt more often"));
+            Ghosts.Add(new GhostType("Hantu", Evidence.GhostOrb, Evidence.FreezingTemperatures, Evidence.Fingerprints, "Moves slower in warmer areas", "Attacks more often and moves faster when it is cold"));
+            Ghosts.Add(new GhostType("Goryo", Evidence.EMF5, Evidence.DotsProjector, Evidence.Fingerprints, "D.O.T.S.-evidence/itself shows only on cam with nobody nearby", "Rarely seen far from place of death"));
+            Ghosts.Add(new GhostType("Myling", Evidence.EMF5, Evidence.GhostWriting, Evidence.Fingerprints, "Very vocal and active, silent during hunts", "Frequently makes paranormal sounds"));
 
-            Ghosts.Add(new GhostType("Yokai", "Ghost Orb", "Spirit Box", "D.O.T.S Projector", "While hunting, it can only hear voices near it", "Talking near the ghost makes it to hunt more often"));
-            Ghosts.Add(new GhostType("Hantu", "Ghost Orb", "Freezing Temperatures", "Fingerprints", "Moves slower in warmer areas", "Attacks more often and moves faster when it is cold"));
-            Ghosts.Add(new GhostType("Goryo", "EMF5", "D.O.T.S Projector", "Fingerprints", "D.O.T.S.-evidence/itself shows only on cam with nobody nearby", "Rarely seen far from place of death"));
-            Ghosts.Add(new GhostType("Myling", "EMF5", "Ghost Writing", "Fingerprints", "Very vocal and active, silent during hunts", "Frequently makes paranormal sounds"));
+            Ghosts.Add(new GhostType("Onryo", Evidence.SpiritBox, Evidence.GhostOrb, Evidence.FreezingTemperatures, "Fears fire and less likely to hunt when threatened", "Extinguishing a flame can cause a hunt"));
+            Ghosts.Add(new GhostType("The Twins", Evidence.EMF5, Evidence.SpiritBox, Evidence.FreezingTemperatures, "Often interacts at the same time", "Either one of them can start a hunt"));
+            Ghosts.Add(new GhostType("Raiju", Evidence.EMF5, Evidence.GhostOrb, Evidence.DotsProjector, "Disrupts electronic devices during hunts", "Siphons power from eletronic devices to move faster"));
+            Ghosts.Add(new GhostType("Obake", "EMF5", Evidence.Fingerprints, Evidence.GhostOrb, "Shapeshifting will leave behind unique evidence", "When intereacting with environment, it will rarely leave a trace"));
 
-            Ghosts.Add(new GhostType("Onryo", "Spirit Box", "Ghost Orb", "Freezing Temperatures", "Fears fire and less likely to hunt when threatened", "Extinguishing a flame can cause a hunt"));
-            Ghosts.Add(new GhostType("The Twins", "EMF5", "Spirit Box", "Freezing Temperatures", "Often interacts at the same time", "Either one of them can start a hunt"));
-            Ghosts.Add(new GhostType("Raiju", "EMF5", "Ghost Orb", "D.O.T.S Projector", "Disrupts electronic devices during hunts", "Siphons power from eletronic devices to move faster"));
-            Ghosts.Add(new GhostType("Obake", "EMF5", "Fingerprints", "Ghost Orb", "Shapeshifting will leave behind unique evidence", "When intereacting with environment, it will rarely leave a trace"));
-
+            // CLONE GHOSTS
             Potential = new List<GhostType>(Ghosts);
-            EvidenceNotNeededHelper = new List<string>();
-            FirstEvidence.ItemsSource = Evidence;
-            SecondEvidence.ItemsSource = Evidence;
-            ThirdEvidence.ItemsSource = Evidence;
-            OutputListView.ItemsSource = Potential;
-            StrengthListView.ItemsSource = Potential;
-            WeaknessListView.ItemsSource = Potential;
-            MissingListView.ItemsSource = Missing;
-            EvidenceNotNeededListView.ItemsSource = EvidenceNotNeededHelper;
+
+            // CHECK FOR NOT NEEDED EVIDENCE
+            EvidenceNotNeeded = new List<string>();
+
+            // SOURCEBINDS
+            lstName.ItemsSource = Potential;
+            FirstEvidence.ItemsSource = Evidences;
+            SecondEvidence.ItemsSource = Evidences;
+            ThirdEvidence.ItemsSource = Evidences;
+            lstWeaknesses.ItemsSource = Potential;
+            lstStrengths.ItemsSource = Potential;
+            lstNotNeeded.ItemsSource = EvidenceNotNeeded;
         }
 
         private void FirstEvidence_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (FirstEvidence.SelectedItem != null)
             {
-                EvidenceNotNeededHelper.Remove(FirstEvidence.SelectedItem.ToString());
+                EvidenceNotNeeded.Remove(FirstEvidence.SelectedItem.ToString());
                 _FirstEvidence = FirstEvidence.SelectedItem.ToString();
-                SearchGhosts(_FirstEvidence);
+                SearchGhostsThatArePossible(_FirstEvidence);
 
             }
 
@@ -89,9 +91,9 @@ namespace Phasmo
         {
             if (SecondEvidence.SelectedItem != null)
             {
-                EvidenceNotNeededHelper.Remove(SecondEvidence.SelectedItem.ToString());
+                EvidenceNotNeeded.Remove(SecondEvidence.SelectedItem.ToString());
                 _SecondEvidence = SecondEvidence.SelectedItem.ToString();
-                SearchGhosts(_SecondEvidence);
+                SearchGhostsThatArePossible(_SecondEvidence);
             }
 
         }
@@ -100,9 +102,9 @@ namespace Phasmo
         {
             if (ThirdEvidence.SelectedItem != null)
             {
-                EvidenceNotNeededHelper.Remove(ThirdEvidence.SelectedItem.ToString());
+                EvidenceNotNeeded.Remove(ThirdEvidence.SelectedItem.ToString());
                 _ThirdEvidence = ThirdEvidence.SelectedItem.ToString();
-                SearchGhosts(_ThirdEvidence);
+                SearchGhostsThatArePossible(_ThirdEvidence);
             }
 
         }
@@ -120,14 +122,14 @@ namespace Phasmo
             _ThirdEvidence = "";
             Potential = new List<GhostType>(Ghosts);
             Missing = null;
-            EvidenceNotNeededHelper = new List<string>();
-            OutputListView.ItemsSource = Potential;
-            StrengthListView.ItemsSource = Potential;
-            WeaknessListView.ItemsSource = Potential;
-            MissingListView.ItemsSource = Missing;
-            EvidenceNotNeededListView.ItemsSource = EvidenceNotNeededHelper;
+            EvidenceNotNeeded = new List<string>();
+            lstName.ItemsSource = Potential;
+            lstWeaknesses.ItemsSource = Potential;
+            lstStrengths.ItemsSource = Potential;
+            lstEvidenceNeeded.ItemsSource = Missing;
+            lstNotNeeded.ItemsSource = EvidenceNotNeeded;
         }
-        private void SearchGhosts(string evidence)
+        private void SearchGhostsThatArePossible(string evidence)
         {
             for (int i = Potential.Count - 1; i >= 0; i--)
             {
@@ -144,20 +146,28 @@ namespace Phasmo
                     Potential.Remove(Potential[i]);
                 }
             }
-            StrengthListView.Items.Refresh();
-            WeaknessListView.Items.Refresh();
-            OutputListView.Items.Refresh();
-            FindMissing();
+            lstWeaknesses.Items.Refresh();
+            lstStrengths.Items.Refresh();
+            lstName.Items.Refresh();
+            FindMissingEvidenceForGhostTypesAndNotNeededEvidence();
+            FilterEvidenceThatIsFoundAlreadyForNotNeeded(_FirstEvidence);
+            FilterEvidenceThatIsFoundAlreadyForNotNeeded(_SecondEvidence);
+            FilterEvidenceThatIsFoundAlreadyForNotNeeded(_ThirdEvidence);
+            lstNotNeeded.ItemsSource = EvidenceNotNeeded;
+            lstNotNeeded.Items.Refresh();
 
         }
-        private void FindMissing()
+        private void FindMissingEvidenceForGhostTypesAndNotNeededEvidence()
         {
-            EvidenceNotNeededHelper = new List<string>(Evidence);
-            Missing =new string[Potential.Count];
+            EvidenceNotNeeded = new List<string>(Evidences);
+            Missing = new string[Potential.Count];
+
+            
             for (int i = 0; i < Missing.Length; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
+                    // FIND ALL EVIDENCES THAT ARE MISSING FOR EACH GHOST TYPE AND ADD THEM TO A STRING
                     if (!(_FirstEvidence == Potential[i].Evidence[j] || _SecondEvidence == Potential[i].Evidence[j] || _ThirdEvidence == Potential[i].Evidence[j]))
                     {
                         if (Missing[i] == null)
@@ -168,12 +178,12 @@ namespace Phasmo
                         {
                             Missing[i] += " - " + Potential[i].Evidence[j];
                         }
-                        string result = EvidenceNotNeededHelper.FirstOrDefault(stringToCheck => stringToCheck.Contains(Potential[i].Evidence[j]));
 
-
+                        // IF EVIDENCE IS FOUND IN THE STRING, REMOVE IT FROM THE LIST BECAUSE IT IS POTENTIAL EVIDENCE
+                        string result = EvidenceNotNeeded.FirstOrDefault(stringToCheck => stringToCheck.Contains(Potential[i].Evidence[j]));
                         if (result != null)
                         {
-                            EvidenceNotNeededHelper.Remove(result);
+                            EvidenceNotNeeded.Remove(result);
                         }
 
 
@@ -181,22 +191,18 @@ namespace Phasmo
                 }
 
             }
-            FilterEvidence(_FirstEvidence);
-            FilterEvidence(_SecondEvidence);
-            FilterEvidence(_ThirdEvidence);
-            MissingListView.ItemsSource = Missing;
-            MissingListView.Items.Refresh();
-            EvidenceNotNeededListView.ItemsSource = EvidenceNotNeededHelper;
-            EvidenceNotNeededListView.Items.Refresh();
+            lstEvidenceNeeded.ItemsSource = Missing;
+            lstEvidenceNeeded.Items.Refresh();
+            
         }
-        private void FilterEvidence(string test)
+        private void FilterEvidenceThatIsFoundAlreadyForNotNeeded(string evidence)
         {
-            if (test != "")
+            if (evidence != "")
             {
-                string result = EvidenceNotNeededHelper.FirstOrDefault(stringToCheck => stringToCheck.Contains(test));
+                string result = EvidenceNotNeeded.FirstOrDefault(stringToCheck => stringToCheck.Contains(evidence));
                 if (result != "")
                 {
-                    EvidenceNotNeededHelper.Remove(result);
+                    EvidenceNotNeeded.Remove(result);
                 }
             }
 
